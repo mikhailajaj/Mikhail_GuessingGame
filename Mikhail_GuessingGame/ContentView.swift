a69147f561d879dad2095c1f5e6b6d33c8cff884
mikhail.ajaj <michelajaj96@gmail.com>
2020-10-23 08:26:41 -0400

    Initial Commit

---
+++ Mikhail_GuessingGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mikhail_GuessingGame
+//
+//  Created by Mikhail on 2020-10-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
