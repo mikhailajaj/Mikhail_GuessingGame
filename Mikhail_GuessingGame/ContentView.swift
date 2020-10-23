fb8f9583a6caf1a2d067cd11fe353cdbe459b2d1
mikhail.ajaj <michelajaj96@gmail.com>
2020-10-23 09:49:34 -0400

    view done

--- Mikhail_GuessingGame/ContentView.swift
+++ Mikhail_GuessingGame/ContentView.swift
@@ -8,10 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var guessedNumber : String = ""
+    @State private var numberOfAttempts : Int = 5
+    @State private var scoreTapped : Bool = false
+    @State private var message : String = ""
+    @State private var randomNumber : Int
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Text("Guessing Game").font(.system(size: 30)).padding().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            Text("I have a number between 1 to 25. You have 5 attempts.\nCan  You guess the number correctly?\n").font(.system(size: 30))
+            TextField("Enter your guessed number",text:$guessedNumber).font(.system(size: 30))
+            Text("\(numberOfAttempts) attempts left").font(.system(size: 30))
+            Button (action: {
+                //check if the number is correct
+                if(Int(guessedNumber) == randomNumber ){
+                    //alert Good job
+                }else if(Int(guessedNumber) ?? 0 > randomNumber){
+                    //the number is too high
+                }else if(Int(guessedNumber) ?? 0 < randomNumber){
+                    //the number is too low
+                }
+                if (numberOfAttempts == 1){
+                    //Game over
+                    //number of attempts = 5
+                }
+                else{
+                    //number of attempt --
+                }
+                
+                
+            }, label: {
+                Text("Check Answer")
+                
+            })
+            Text(message).font(.system(size: 30))
+            }//VSTACK
+            .navigationBarTitle("Mikhail_GuessingGame", displayMode: .inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(
+                trailing: Button(action: {
+                    self.scoreTapped = true
+                    
+                })
+                {
+                    Text("SCORE")
+                })
+            .sheet(isPresented: $scoreTapped)
+            {
+                //view the view of score
+            }
+            
+        }//NavigationView
+        .onAppear(){
+            
+        }
+        .navigationViewStyle(DefaultNavigationViewStyle())
+        .colorScheme(.dark)
+    }//body
+    private func setRandomNumber(){
+        randomNumber = Int.random(in: 1...25)
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
